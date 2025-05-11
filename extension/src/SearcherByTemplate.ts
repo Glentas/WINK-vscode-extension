60bfa652f19e8fb56911e74415186f369c8226cb
MYNAMEIS <you@example.com>
2025-05-11 15:42:24 +0300

    Fixed search by template

--- extension/src/SearcherByTemplate.ts
+++ extension/src/SearcherByTemplate.ts
@@ -13,35 +13,65 @@ export class SearcherByTemplate {
         console.log("findByScsTemplate, scsTemplate:", scsTemplate);
         console.log("findByScsTemplate, structWinkIdtf:", structWinkIdtf);
 
-        //returns array with scAddrs which are correspond to template
+        //for testing purposes
+        // const my_template = new ScTemplate();
+        // const u = this.scClient.searchKeynodes("set");
+
+        
+        // const search_addr = new ScAddr((await u).set.value);
+        // console.log("findByScsTemplate, u:", search_addr.value);
+        // const params = {["set"]: search_addr,};
+
+        //my_template.triple(search_addr, ScType.VarPermPosArc, [ScType.VarNode, "_a"]);
+        
         const searchingResults = await this.scClient.searchByTemplate(scsTemplate);
         console.log("findByScsTemplate, searchingResults:", searchingResults);
 
-        // from found scAddrs we collect theirs int values
         const uniqNodes = new Set<number>();
-        searchingResults.forEach(e => e.forEachTriple(n => uniqNodes.add(n.value)));
+        for(const i of searchingResults.values())
+        {
+            for(let j = 0; j < i.size; j++)
+            {
+                uniqNodes.add(i.get(j).value);
+            }
+        }
 
-        // sctructure which contains found construstions
-        const structNode = (await this.scClient.resolveKeynodes([{id: structWinkIdtf, type: ScType.ConstNodeStructure}]))[0];
+        console.log("findByScsTemplate, found nodes:", uniqNodes);
+
+        const keynodes = [
+            { id: structWinkIdtf, type: ScType.NodeStructure },
+          ];
+      
+        const result_of_resolve = await this.scClient.resolveKeynodes(keynodes);
+        const structNode = result_of_resolve[structWinkIdtf];
+        console.log("findByScsTemplate, generated structNode:", structNode);
+
         const construction = new ScConstruction();
-
-        // connect found constructions to structure
-        uniqNodes.forEach(node => {
+        
+        for(const i of uniqNodes)
+        {   
+            const temp_addr = new ScAddr(i);
+            
             construction.generateConnector(
                 ScType.ConstPermPosArc,
                 structNode,
-                new ScAddr(node)
+                temp_addr
             );
-            console.log("findByScsTemplate, found and generated nodes:", node);
-        });
+
+            console.log("findByScsTemplate, generated connector:", i);
+        }
 
         if(uniqNodes.size == 0)
         {
             console.log("findByScsTemplate, found no structures");
         }
 
+        console.log("findByScsTemplate, generating construction...", construction);
 
         await this.scClient.generateElements(construction);
+
+        console.log("findByScsTemplate, generation is succesfull");
+
         return structWinkIdtf;
     }
 }
